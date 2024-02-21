a04af2f61a43f7235a776939b7809a0e1846452a
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-02-21 11:30:07 +0330

    single line plot

--- generating data/simple_line.py
+++ generating data/simple_line.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 
 squares = [1,4,9,16,25]
+input_value = [1,2,3,4,5]
+# style the plot
+plt.style.use("seaborn-v0_8")
 fig , ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_value,squares , linewidth = 3)
+# set chart title and axie
+ax.set_title("Squre Numbers" , fontsize = 24)
+ax.set_xlabel("Value" , fontsize = 14)
+ax.set_ylabel("Squre of value" , fontsize = 14)
+# set size of tick lables
+ax.tick_params(labelsize =14)
+
 plt.show()
